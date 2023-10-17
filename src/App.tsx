a1505ee02619f1049c96ba9d29055b5f8bb1fff3
Marc Stedman <marc@starshiphsa.com>
2023-10-17 15:58:17 -0400

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,16 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-       <div>
-            <h1>Starship Todo List</h1>
-            <div className="form">
-                <input className="input" type="text" placeholder="Add your task" />
-                <button className="button">Submit</button>
-            </div>
+        <h1>Starship Todo List</h1>
 
-            <ul  className="list">
-                <li>Walk the dog</li>
-            </ul>
-
+        <div className="form">
+            <input className="input" type="text" placeholder="Add your task" />
+            <button className="button">Submit</button>
         </div>
+
+        <ul  className="list">
+            <li>Walk the dog</li>
+        </ul>
     </div>
   );
 }
